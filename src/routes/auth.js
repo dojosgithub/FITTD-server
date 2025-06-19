ac7eb2d77e25a7aad6b404d2e771fd1c84285869
Basil Jamil <118303446+basiljamil12@users.noreply.github.com>
2025-06-19 18:05:12 +0500

    Added Authenticate() in changePassword api

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -3,7 +3,7 @@ import { Router } from 'express'
 
 // * Controllers
 import { CONTROLLER_AUTH } from '../controllers'
-import { permitMiddleware, Authenticate } from '../middlewares'
+import { Authenticate } from '../middlewares'
 
 const router = Router()
 
@@ -14,7 +14,7 @@ router.post('/signin', CONTROLLER_AUTH.signIn)
 router.post('/signout', Authenticate(), CONTROLLER_AUTH.signOut)
 router.post('/verify-otp', CONTROLLER_AUTH.verifyOtp)
 router.post('/forgot-password', CONTROLLER_AUTH.forgotPassword)
-router.post('/change-password', CONTROLLER_AUTH.changePassword)
+router.post('/change-password', Authenticate(), CONTROLLER_AUTH.changePassword)
 router.delete('/delete-account', Authenticate(), CONTROLLER_AUTH.deleteAccount)
 
 export default router
