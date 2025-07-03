df0890eb051142bc86e2ac92237de3e3d2780dde
Basil Jamil <118303446+basiljamil12@users.noreply.github.com>
2025-07-03 12:03:15 +0500

    Refactor scraper handler message handling
    Moved the SCRAPING_STARTED_MSG constant from the controller to the helper and centralized the response message logic inside makeScraperHandler. Simplifies controller code by removing the need to pass the message explicitly.

--- src/controllers/scraper.js
+++ src/controllers/scraper.js
@@ -11,15 +11,13 @@ import {
 } from '../services'
 import { makeScraperHandler } from '../utils'
 
-const SCRAPING_STARTED_MSG = 'Scraping started. After few minutes run get all products api to see the results'
-
 export const CONTROLLER_SCRAPER = {
-  getHouseOfCBProducts: asyncMiddleware(makeScraperHandler(scrapeHouseOfCB, SCRAPING_STARTED_MSG)),
-  getEbDenimProducts: asyncMiddleware(makeScraperHandler(scrapeEbDenim, SCRAPING_STARTED_MSG)),
-  getLuluLemonProducts: asyncMiddleware(makeScraperHandler(scrapeLuluLemon, SCRAPING_STARTED_MSG)),
-  getAgoldeMenAndWomenProducts: asyncMiddleware(makeScraperHandler(scrapeAgolde, SCRAPING_STARTED_MSG)),
-  getTheReformationProducts: asyncMiddleware(makeScraperHandler(scrapeTheReformation, SCRAPING_STARTED_MSG)),
-  getSelfPotraitProducts: asyncMiddleware(makeScraperHandler(scrapeSelfPotrait, SCRAPING_STARTED_MSG)),
-  getJCrewProducts: asyncMiddleware(makeScraperHandler(scrapeJCrew, SCRAPING_STARTED_MSG)),
-  getSaboSkirtProducts: asyncMiddleware(makeScraperHandler(scrapeSaboSkirt, SCRAPING_STARTED_MSG)),
+  getHouseOfCBProducts: asyncMiddleware(makeScraperHandler(scrapeHouseOfCB)),
+  getEbDenimProducts: asyncMiddleware(makeScraperHandler(scrapeEbDenim)),
+  getLuluLemonProducts: asyncMiddleware(makeScraperHandler(scrapeLuluLemon)),
+  getAgoldeMenAndWomenProducts: asyncMiddleware(makeScraperHandler(scrapeAgolde)),
+  getTheReformationProducts: asyncMiddleware(makeScraperHandler(scrapeTheReformation)),
+  getSelfPotraitProducts: asyncMiddleware(makeScraperHandler(scrapeSelfPotrait)),
+  getJCrewProducts: asyncMiddleware(makeScraperHandler(scrapeJCrew)),
+  getSaboSkirtProducts: asyncMiddleware(makeScraperHandler(scrapeSaboSkirt)),
 }
--- src/utils/scrapeHelpers/scraperControllerHelper.js
+++ src/utils/scrapeHelpers/scraperControllerHelper.js
@@ -1,9 +1,11 @@
 import { StatusCodes } from 'http-status-codes'
 import { closeGlobalBrowser } from '../../utils'
 
-export function makeScraperHandler(scrapeFn, message) {
+const SCRAPING_STARTED_MSG = 'Scraping started. After few minutes run get all products api to see the results'
+
+export function makeScraperHandler(scrapeFn) {
   return async (req, res) => {
-    res.status(StatusCodes.ACCEPTED).json({ message })
+    res.status(StatusCodes.ACCEPTED).json({ message: SCRAPING_STARTED_MSG })
     try {
       await scrapeFn()
     } finally {
